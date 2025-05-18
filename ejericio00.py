6c358ec956b5f9dfd58e28991f120b65922f9522
MendozaDavidprogam <mendozadavidprogramacion2.0@gmail.com>
2025-05-17 22:33:11 -0400

    primer ejercicio de introduccion

---
+++ ejericio00.py
@@ -0,0 +1,29 @@
+"""
+SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+"""
+
+# URL de python (https://www.python.org/)
+
+# comentario en linea usando #
+
+"""
+comentario en multiples
+lineas con triple comillas 
+"""
+
+"comentario en linea usando comillas"
+
+variable = "soy una variable"
+
+# variable de los datos primitivos
+
+MI_CONSTANTE = "constante pero de conveniencia"
+
+Mi_int = 10 # variable de tipo entero int
+Mi_str = "soy una cadena de texto" # variable strin o cadena de texto str
+Mi_float = 12.5 # variable flotante o float
+Mi_boolean = True # variable de tipo booleno verdadero(True)
+Mi_boolean = False # variable de tipo booleno falso(False)
+
+
+print("hola, mi nombre es david y estoy usando python.")
